95107e2ddcc2634c90f413af5d916ea4bf3c99cb
kabirshuvo <kabirshuvo19@gmail.com>
2025-07-28 21:57:25 +0600

    redux included

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import testReducer from './features/testSlice';
 
-// Example: Empty reducer
 export const store = configureStore({
   reducer: {
-    // products: productsReducer, // add later
+    test: testReducer,
   },
 });
 
-// Infer types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
